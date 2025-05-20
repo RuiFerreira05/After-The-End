db6c49c21fb8ac10ae3dd8f0822ad794302b8885
RuiFerreira05 <11556654433221m@gmail.com>
2025-05-20 14:22:00 +0100

    finally

---
+++ src/main/java/tps/tp4/structures/S_WoodCuttersLodge.java
@@ -0,0 +1,13 @@
+package tps.tp4.structures;
+
+import tps.tp4.Settings;
+
+public class S_WoodCuttersLodge extends Structure {
+    private static final int[] COST = Settings.WOODCUTTER_LODGE_COST; // wood, stone, metal
+
+    public S_WoodCuttersLodge() {
+        super("woodcutters_lodge", COST);
+        this.production = Settings.WOODCUTTER_LODGE_PRODUCTION;
+        this.description = "Produces " + getWoodProduction() + " wood per day.";
+    }
+}
